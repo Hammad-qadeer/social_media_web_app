a0c8e2ce2955ecdd57c0ea52cfe6d435283fc7c5
Hammad-qadeer <hammadqadeer12@gmail.com>
2022-06-17 21:12:39 +0500

    changes done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Testing the react app in deployement</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+         This is testing the react web app.
+        </a>
+      </header>
     </div>
   );
 }
